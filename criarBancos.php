b5b885e995ff3778259d608e254ba334f2151a2e
Erik Mota <121909102+serikdev@users.noreply.github.com>
2023-01-25 11:30:22 -0300

    Add files via upload

---
+++ criarBancos.php
@@ -0,0 +1,10 @@
+<?php
+$bdServidor = 'localhost';
+$bdUsuario = 'sistemaGerenciador';
+$bdSenha = 'sistema';
+$bdBanco = 'dados';
+
+$conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+
+
+?>
